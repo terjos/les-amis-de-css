1afcc3aade021208761660280056e21eb1ad60a6
terjos <terjos@neuf.fr>
2022-02-21 08:40:39 +0100

    chore(build): config vite and add stylelinter

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  files: ["src/**/*.css"],
+  extends: "stylelint-config-twbs-bootstrap",
+  rules: {
+    "number-leading-zero": "always",
+  },
+};
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import viteStylelint from "@amatlash/vite-plugin-stylelint";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
+export default defineConfig({
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        conferences: resolve(root, "conferences.html"),
+      },
+    },
+  },
+  plugins: [
+    viteStylelint({
+      include: ["**/*.css"],
+    }),
+  ],
+});
