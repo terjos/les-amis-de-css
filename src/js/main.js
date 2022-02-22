e9a4efe4d459884673b11829715a9560d6b94528
terjos <terjos@neuf.fr>
2022-02-22 15:22:53 +0100

    feat(conferences page) edit html, css and js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const conferences = [...document.querySelectorAll(".conferences-card")];
+
+if (conferences.length > 0) {
+  for (let i = 0; i < conferences.length; i++) {
+    const card = conferences[i];
+    card.style.opacity = 0;
+    card.style.transform = "translateX(50px)";
+    setTimeout(() => {
+      card.style.transitionDuration = "1s";
+      card.style.opacity = 1;
+      card.style.transform = "translateX(0)";
+    }, 200 * (i + 1));
+  }
+}
